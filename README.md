db358c9b0ec148f92baef0a9110335650c0f4013
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-12 16:52:35 +0300

    Add print count of vacancies for different languages

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# USEFUL LINKS:
+```
+https://api.hh.ru/areas/  | Получение всех айдищников городов
+https://api.hh.ru/professional_roles/  | Получение айдишников  для профессий
+```
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from pprint import pprint
+
 import requests
 
 
+def get_programmers_vacancies() -> dict:
+    programmers_id = 96
+    city_id = 1
+
+    url = 'https://api.hh.ru/vacancies'
+    programmers_languages = ['Javascript', 'Java', 'Python', 'Ruby', 'PHP',
+                             'C++', 'C#', 'C', 'GO', 'Scala', 'Swift',
+                             'Typescript']
+    languages_vacancies = {}
+
+    for language in programmers_languages:
+        params = {
+            'text': f'Программист {language}',
+            'area': city_id,
+            'professional_role': programmers_id,
+            'per_page': 20,
+            'archived': False,
+            'search_period': 30
+        }
+
+        response = requests.get(url=url, params=params)
+        response.raise_for_status()
+        vacancies = response.json()
+        languages_vacancies[language] = vacancies['found']
+
+    return languages_vacancies
+
+
 if __name__ == '__main__':
-    pass
+    pprint(get_programmers_vacancies())
